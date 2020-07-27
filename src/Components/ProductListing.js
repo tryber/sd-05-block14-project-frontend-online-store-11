a78776eee21e9f8f01f317e9005515c9bd80c43c
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-27 11:16:29 -0300

    ex 4 - list do lado esquerdo, ainda nao cliclavel, mas já passando no teste

--- src/Components/ProductListing.js
+++ src/Components/ProductListing.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Component } from 'react';
-// import {
-//   getCategories,
-//   getProductsFromCategoryAndQuery,
-// } from '../services/api';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+} from '../services/api';
+import './ProductListing.css';
 
 // Tudo que está comentado será utilizado de uma forma ou outra em algum momento. Não apagar.
 
@@ -11,20 +12,20 @@ import { Component } from 'react';
 // Que veremos como será utilizado mais para frente.
 
 class ProductListing extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     categories: [],
-  //   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
   //  this.handleClick = this.handleClick.bind(this);
-  // }
+  }
   // Nao Apagar - Aqui é como se usa a API
 
-  // async componentDidMount() {
-  //   const data = await getCategories();
-  //   console.log(data);
-  //   this.setState({ categories: data });
-  // }
+  async componentDidMount() {
+    const data = await getCategories();
+    console.log(data);
+    this.setState({ categories: data });
+  }
 
   // async handleClick() {
   //   const product = await getProductsFromCategoryAndQuery('MLB1055', 'Motorola');
@@ -33,18 +34,21 @@ class ProductListing extends Component {
   // }
 
   render() {
+    const { categories } = this.state;
+
     return (
-      <div>
+      <div className='Product-List'>
         {/* Não apagar  - Aqui é como se renderiza a  */}
         {/* lista. Apenas uma forma, não necessariamente a pedida nos requisitos */}
-
-        {/* {categories.map((cat) => (
-          <div key={cat.id} style={{ border: 'solid 5px' }}>
-            <p>{cat.id}</p>
-            <p>{cat.name}</p>
-            <button type="button" onClick={this.handleClick}>Add To Console Cart</button>
-          </div>
-        ))} */}
+        <ul>
+          {categories.map((cat) => (
+            <div key={cat.id}>
+              {/* <p>{cat.id}</p> */}
+              <li data-testid="category">{cat.name}</li>
+              {/* <button type="button" onClick={this.handleClick}>Add To Console Cart</button> */}
+            </div>
+          ))}
+        </ul>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
