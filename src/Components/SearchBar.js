a078658f63b77cd859d3dd221902536548bc2c34
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-28 10:45:39 -0300

    ...

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,12 +5,16 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-area">
-        <input type="text" placeholder="Digite algum termo de pesquisa ou escolha uma categoria." />
-        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <input
+          type="text"
+          placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
+        />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
       </div>
     );
   }
 }
-
 
 export default SearchBar;
