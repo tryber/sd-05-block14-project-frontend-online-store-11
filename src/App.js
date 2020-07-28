03bd7aa4fa2c36bd940b682aeb6f7691e19eb7df
Preto115 <preto115@gmail.com>
2020-07-28 11:34:19 -0300

    salvando implementacoes do carrinho antes de dar um pull

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import ProductListing from './Components/ProductListing';
+import Carrinho from './Components/Carrinho';
+
+// array para simular os produtos adicionado ao carrinho
+const produto = [
+  {
+    produto: 'camisa',
+    quantidade: 1,
+    preco: '99,90',
+  },
+  {
+    produto: 'chuteiras',
+    quantidade: 2,
+    preco: '79,90',
+  },
+];
 
 class App extends Component {
   // constructor(props) {
@@ -11,8 +26,12 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <header className="App-header">
+          <Link to="/carrinho"><img data-testid="shopping-cart-button" src="../cart-1.png" alt="carrinho de compras" height="50px" /></Link>
+        </header>
         <Switch>
-          <Route path="/" component={ProductListing} />
+          <Route exact path="/" component={ProductListing} />
+          <Route path="/carrinho/" render={(props) => <Carrinho {...props} produto={produto} />} />
         </Switch>
       </BrowserRouter>
     );
